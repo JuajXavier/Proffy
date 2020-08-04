d3aad728b7fc8df9669c05fe812330c1a519bd13
João Xavier <jvitorxavier@gmail.com>
2020-08-04 18:50:04 -0300

    Routes and database connections working

---
+++ backend/src/controllers/ClassesController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes';
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+
+export default class ClassesController {
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    if(!filters.week_day ||!filters.subject ||!filters.time) {
+      return res.status(400).json({
+        error: "Missing filters to search classes"
+      })
+    }
+  }
+  
+  async create (req: Request, res: Response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule
+    } = req.body;
+  
+    const trx = await db.transaction();
+  
+    try {
+      const insertedUsersIds = await trx('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+      });
+    
+      const user_id = insertedUsersIds[0];
+    
+      const insertedClassesId = await trx('classes').insert({
+        subject,
+        cost,
+        user_id
+      });
+    
+      const class_id = insertedClassesId[0];
+    
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem)  => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to),
+        };
+      })
+    
+      await trx('class_schedule').insert(classSchedule);
+    
+      await trx.commit();
+    
+      return res.status(201).send();
+    } catch (err) {
+      await trx.rollback();
+  
+      return res.status(400).json({
+        error: 'Unexpected error while creating new class'
+      })
+    }
+  }
+}
